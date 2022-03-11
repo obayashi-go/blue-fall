7f826aa09a926905d4170266a10afc74e163823e
obayashi-go <forest.go428@outlook.jp>
2022-03-11 20:25:00 +0900

    Firebase

---
+++ firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyB_0AR3NEQujUtgF0Dqw8VWUBqdBAGOC4M",
+    authDomain: "blue-fall-2022.firebaseapp.com",
+    databaseURL: "blue-fall-2022",
+    storageBucket: "blue-fall-2022.appspot.com",
+    messagingSenderId: "812250998022",
+    appId: "1:812250998022:web:35ac21268c05e4db1daa87"
+};
